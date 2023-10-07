ea876acee1a66c12b7459aa2cd678f2cccebe79d
TommyRazer <139071410+TommyRazer@users.noreply.github.com>
2023-10-07 15:21:25 -0300

    Delete Nombre De Usuario.js

--- Nombre De Usuario.js
+++
@@ -1,6 +0,0 @@
-// Después de verificar las credenciales en index.html
-if (credencialesSonCorrectas) {
-    const nombreUsuario = "Tommy"; // Aquí obtén el nombre de usuario de tu lógica de autenticación
-    window.location.href = 'dashboard.html?username=Tommy' + encodeURIComponent(nombreUsuario);
-}
-    
